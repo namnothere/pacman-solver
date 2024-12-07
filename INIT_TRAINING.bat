8222e5e1d6be5ee4efd3eccd5b6dd114c93d9667
nem <canhnamtran216@gmail.com>
2024-12-07 11:07:46 +0700

    feat: model

--- INIT_TRAINING.bat
+++ INIT_TRAINING.bat
@@ -1,1 +1,2 @@
 python stable_baselines3_example.py --timesteps=100_000 --save_model_path=model.zip
+python stable_baselines3_example.py --timesteps=10_000 --onnx_export_path=model.onnx --save_model_path=model.zip
--- RESUME_TRAINING.bat
+++ RESUME_TRAINING.bat
@@ -1,1 +1,2 @@
-python stable_baselines3_example.py --timesteps=100_000 --resume_model_path=model.zip --inference
+python stable_baselines3_example.py --timesteps=1_500 --resume_model_path=model.zip
+python stable_baselines3_example.py --timesteps=10_000 --save_model_path=model.zip --resume_model_path=model.zip
